b7400895ca79857ab601cd4f5b6784679d643e81
LiShaojun1126 <yur1loverslave@gmail.com>
2025-05-24 15:20:17 -0400

    tasks all completed

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get("/", (req, res) => {
 // CREATE GET /health
 app.get("/health", (req, res) => {
   // Return JSON: { status: "ok" }
+  res.json({ status: "ok"});
 });
 
 // TASK 2: User Routes
@@ -33,22 +34,55 @@ const users = [
 
 app.get("/users", (req, res) => {
   // Return all users
+  res.json(users);
 });
 
 app.get("/users/:id", (req, res) => {
   // 1. Get ID from req.params
+  
   // 2. Find user in array
+    const {id} = req.params;// this will return a STRING id!!!!
+    const numberId = parseInt(id);
+    //console.log(numberId);
+
+    const userFound = users.find(item => item.id === numberId);
+    //console.log(typeof(userFound));
+
+  if(userFound !== undefined){
+    res.json(userFound);
+  }
+  else{
+    res.status(404).json({status: "User not found"});
+  }
   // 3. Return user or 404 if not found
+
+
 });
 
 // TASK 3: Message Submission
-app.post("/messages", (req, res) => {
+app.post("/messages", (req, res) => {  //set the postman to POST function, and you need to provide message manually
   // 1. Get text from req.body
   // 2. Validate text exists
   // 3. Return JSON with:
   //    - Generated ID (number)
   //    - Original text
   //    - status: "received"
+
+  const {text} = req.body;
+  console.log(text)
+  if(text){
+    const result = {
+      id: Math.floor(Math.random() * 101),
+      message: text,
+      status: "received"
+    }
+
+    res.send(result);
+  }
+  else{
+    res.status(404).json({status: "There is no text"});
+   }
+
 });
 
 // ------------------------------------------------
